d5338f8be061b2ca211f481973ef67c179fb6cfb
Prince Kumar <89452166+princekumar93@users.noreply.github.com>
2023-10-31 03:41:58 -0400

    Added edit/delete buttons App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import { faker } from '@faker-js/faker';
 import "./App.css";
 
 function App() {
@@ -19,14 +20,39 @@ function App() {
     }
   ]
 
-  const tblEmployees = employees.map((employee, id) => {
+  function createRandomUser() {
+    return {
+      userId: faker.string.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+    };
+  }
+  
+  const USERS = faker.helpers.multiple(createRandomUser, {
+    count: 5,
+  });
+
+  function editButton(){
+    
+  }
+
+  function deleteButton(){
+
+  }
+
+  const buttons = {
+    editButton : <button className="btn btn-primary" onClick={editButton}>Edit</button>,
+    deleteButton: <button className="btn btn-danger" onClick={deleteButton}>Delete</button>
+  }
+
+  const tblUser = USERS.map((user, idx) => {
     return (
-      <tr key={id}>
-        <th>{employee.SrNo}</th>
-        <td>{employee.FirstName}</td>
-        <td>{employee.LastName}</td>
-        <td>{employee.EmailId}</td>
-        <td>{employee.Salary}</td>
+      <tr key={user.userId}>
+        <th>{idx + 1}</th>
+        <td>{user.username}</td>
+        <td>{user.email}</td>
+        <td>{user.email}</td>
+        <td>{buttons.editButton} {buttons.deleteButton}</td>
       </tr>
     );
   });
@@ -40,14 +66,13 @@ function App() {
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
+            <th scope="col">Name</th>
             <th scope="col">Email</th>
-            <th scope="col">Salary</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
-          {tblEmployees}
+          {tblUser}
         </tbody>
       </table>
     </div>
